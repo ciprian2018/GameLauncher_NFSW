cc9d05c9449b20944b1c2ef57d1c7ee28e5c971c
metonator <metonator@gmail.com>
2018-04-03 19:14:47 +0200

    Hello world?

---
+++ GameLauncherWPF/GameLauncherWPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("GameLauncher")]
+[assembly: AssemblyDescription("GameLauncher for Need For Speed(TM): World")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SBRW Team")]
+[assembly: AssemblyProduct("GameLauncher")]
+[assembly: AssemblyCopyright("Copyright © Leorblx/MeTonaTOR 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.9.2.0")] //AKA: Early 2.0.0.0
+[assembly: AssemblyFileVersion("1.9.2.0")] //AKA: Early 2.0.0.0
